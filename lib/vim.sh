10f31424e6a9f5dfa01372b62aea1c9e9879e302
Phil Ingram <pingram.au@gmail.com>
2017-01-10 18:39:47 +1030

    vim: fix vim lib

--- lib/vim.sh
+++ lib/vim.sh
@@ -10,8 +10,8 @@ fi
 # on the file extension.
 #
 # Bash
-[[ $(grep -c vim-template\.sh ${VIMRC} || true) == 0 ]] \
+[[ $(grep -c template\.sh ${VIMRC} || true) == 0 ]] \
   && echo "autocmd BufNewFile *.sh 0r ${ENV_ROOT}/lib/vim/template.sh" >> ${VIMRC}
 # Ruby
-[[ $(grep -c vim-template\.rb ${VIMRC} || true) == 0 ]] \
+[[ $(grep -c template\.rb ${VIMRC} || true) == 0 ]] \
   && echo "autocmd BufNewFile *.rb 0r ${ENV_ROOT}/lib/vim/template.rb" >> ${VIMRC}
