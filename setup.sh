78ec425f5a6bc08882826ede43c10463f7e59352
Phil Ingram <pingram.au@gmail.com>
2017-01-10 18:40:13 +1030

    bashate: fix to 4 space tab stops

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ SOURCE="source \${HOME}/.bash/bashrc"
 BASHENV_SOURCE_PATH="$(dirname $(readlink -f $0))"
 
 PACKAGES=(
-  python3-bashate
-  rubygem-bundler
-  ruby-irb
+    python3-bashate
+    rubygem-bundler
+    ruby-irb
 )
 
 echo "****************************************************"
@@ -27,35 +27,35 @@ read
 # If it doesn't exist we default to quickly symlinking to make everything work.
 echo -n "Checking ~/.bash directory: "
 [[ ! -d "${HOME}/.bash" ]] \
-  && echo "Creating BashEnv symlink" \
-  && ln -vs "${BASHENV_SOURCE_PATH}" "${HOME}/.bash" \
-  || echo "OK"
+    && echo "Creating BashEnv symlink" \
+    && ln -vs "${BASHENV_SOURCE_PATH}" "${HOME}/.bash" \
+    || echo "OK"
 
 # To reduce clutter in ~/.bashrc we just put in a single source that makes all
 # the things JustWork™
 echo -n "Checking ~/.bashrc: "
 [[ $(grep -c "${SOURCE}" ${BASHRC} || true) == 0 ]] \
-  && echo "Adding a source to ~/.bashrc" \
-  && echo -e "\n# BashEnv\n${SOURCE}" >> ${BASHRC} \
-  && source ${BASHRC} \
-  || echo "OK"
+    && echo "Adding a source to ~/.bashrc" \
+    && echo -e "\n# BashEnv\n${SOURCE}" >> ${BASHRC} \
+    && source ${BASHRC} \
+    || echo "OK"
 
 # Some rpms are nice to have, some are mandatory
 echo -n "Checking for needed packages: "
 for package in ${PACKAGES[@]}; do
-  rpm -q $package >/dev/null
-  [[ $? -gt 0 ]] \
-    && echo "Package '${package}' is not installed." \
-    && missing+=(${package})
+    rpm -q $package >/dev/null
+    [[ $? -gt 0 ]] \
+        && echo "Package '${package}' is not installed." \
+        && missing+=(${package})
 done
 if [[ -n ${missing} ]]; then
-  echo "Attempt install of packages? (y/N)"
-  for i in ${missing[@]}; do
-    echo "  - ${i}"
-  done
-  read yn
-  [[ ${yn} =~ ^[y|Y]$ ]] \
-    && sudo dnf -y install ${missing[@]}
+    echo "Attempt install of packages? (y/N)"
+    for i in ${missing[@]}; do
+        echo "  - ${i}"
+    done
+    read yn
+    [[ ${yn} =~ ^[y|Y]$ ]] \
+        && sudo dnf -y install ${missing[@]}
 fi
 [[ -z ${missing-} ]] && echo "OK"
 
@@ -63,18 +63,18 @@ fi
 # don't let friends use bash hashes..
 echo -n "Checking ruby gems: "
 pushd ${BASHENV_SOURCE_PATH} > /dev/null
-  if [[ -f Gemfile.lock ]]; then
-    gems=($(grep ^gem Gemfile | tr -d "'" | awk '{print $NF}'))
-    for gem in ${gems}; do
-      [[ $(grep -c ${gem} Gemfile.lock) == 0 ]] \
-        && echo "Missing gem '${gem}'; Installing." \
-        && bundle install \
-        || echo "OK"
-    done
-  else
-    echo "Installing gems"
-    bundle install
-  fi
+    if [[ -f Gemfile.lock ]]; then
+        gems=($(grep ^gem Gemfile | tr -d "'" | awk '{print $NF}'))
+        for gem in ${gems}; do
+            [[ $(grep -c ${gem} Gemfile.lock) == 0 ]] \
+                && echo "Missing gem '${gem}'; Installing." \
+                && bundle install \
+                || echo "OK"
+        done
+    else
+        echo "Installing gems"
+        bundle install
+    fi
 popd > /dev/null
 
 echo
