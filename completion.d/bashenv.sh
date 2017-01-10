7c7aed8d5fd18cbce849b084983f8764e875f7d7
Phil Ingram <pingram.au@gmail.com>
2017-01-11 04:46:54 +1030

    bashate: more fixes

--- completion.d/bashenv.sh
+++ completion.d/bashenv.sh
@@ -4,17 +4,17 @@
 #
 
 _bashenv() {
-  local cur prev option
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  option="-h --help -r --reset -s --setup"
+    local cur prev option
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    option="-h --help -r --reset -s --setup"
 
-  if [ $COMP_CWORD -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "${option}" -- ${cur}) )
-  fi
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $(compgen -W "${option}" -- ${cur}) )
+    fi
 
-  return 0
-} &&
+    return 0
+}
 
 complete -F _bashenv bashenv
